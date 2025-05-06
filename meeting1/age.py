1af7b061f031c18150cd60c32ab57efb911f8b4f
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-05-06 11:01:00 +0300

    initial commit

---
+++ meeting1/age.py
@@ -0,0 +1,9 @@
+name = input("Insert your name: ")
+year = input("Insert your birth year: ")
+year = int(year)
+print(f"Hey {name}! You are {2025-year} years old")
+# print(type(year))
+# year_as_int = int(year)
+# print(type(year_as_int))
+
+# hh:mm:ss
--- meeting1/basics.py
+++ meeting1/basics.py
@@ -1,1 +1,47 @@
 print("Hi there")
+num = 5
+print(num)
+# print("num")
+# print(type(num))
+# nfghlfsjghlkdsjfhgskl
+result = num / 5
+print(result)
+my_type = type(result)
+print(my_type)
+word = "Sun"
+# word / 5
+print(2 ** 5)
+print(5/2)
+print(5//2)
+print(5%2)
+print(5 % 3)
+res = divmod(5, 3)
+print(type(res))
+print(res[0])
+a, b = res
+print(b)
+hours, minutes = divmod(137, 60)
+print(hours)
+print(minutes)
+print("Hours",hours, 'Minutes',minutes)
+sentence = 'He said:\n"The life is " '
+sentence = """He said: 
+"The life is " """
+
+text = f"{hours}:{minutes}"
+print(text)
+print(res)
+
+name = 'Rachely'
+city = "Givataim"
+
+print("My name is", name, "and I live in", city )
+print(f"My name is {name} and I live in {city}")
+
+num1 = 3.738456247658273
+print(f"The num is: {num1:.2f}")
+
+user_input = input("Please insert you input: ")
+print(f"The user inserted {user_input}")
+
+print("bye")
