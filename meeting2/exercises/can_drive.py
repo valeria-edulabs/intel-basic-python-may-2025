c808cdc3dbcddb16a1389144c57f31638cba7fad
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-05-13 10:28:14 +0300

    meeting2

--- meeting2/exercises/can_drive.py
+++ meeting2/exercises/can_drive.py
@@ -1,7 +1,8 @@
 # Exercise:
 
 # Write a program that receives two arguments from the user:
-# - drink (a string representing the alcoholic drink - one of: "beer", "wine", "whiskey", "water")
+# - drink (a string representing the  drink -
+# one of: "beer", "wine", "whiskey", "water")
 # and
 # - amount (an integer representing the amount of drink consumed in milliliters)
 # The program should print "You are allowed to drive" if it is legally allowed to drive,
@@ -11,6 +12,7 @@
 # Wine: 200 ml (1 regular portion)
 # Whiskey: 50 ml (1 regular portion)
 
-# If the amount consumed exceeds the legal portion for that drink type, you are not allowed to drive.
+# If the amount consumed exceeds the legal portion for that drink type,
+# you are not allowed to drive.
 # If the amount consumed is within the legal limit, you are allowed to drive.
 # If no alcohol was consumed (drink == "water"), you are allowed to drive.
