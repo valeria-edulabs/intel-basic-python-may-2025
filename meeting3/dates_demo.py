597070c3af7a3607c04bd64912538fac6d7175a7
Valeria Aynbinder <valeria.aynbinder@gmail.com>
2025-05-20 11:53:12 +0300

    meeting3

--- meeting3/dates_demo.py
+++ meeting3/dates_demo.py
@@ -1,10 +1,18 @@
 import datetime
 
+
+# datetime.datetime
+# datetime.date
+# datetime.time
+# datetime.timedelta
+
 now = datetime.datetime.now()
 print(now)
 print(type(now))
 
+print(now.weekday())
 
+#
 days = {
     0: "Mon",
     1: "Tue",
@@ -15,45 +23,73 @@ days = {
     6:" Sun"
 }
 print(days[now.weekday()])
+print(now.month, now.second)
 
-dt = datetime.datetime(1987, 2, 11)
-print(type(dt))
-print(days[dt.weekday()])
+bday = datetime.datetime(2026,2,11)
+print(bday)
+print(bday - now)
+print(bday < now)
+# print(bday + now)
+print(now + datetime.timedelta(10, hours=37))
 
-#https://strftime.org/
-parsed_date = datetime.datetime.strptime(
-    "2024-02-08, hours: 15:09",
-    "%Y-%m-%d, hours: %H:%M")
-print(parsed_date)
-print(type(parsed_date))
+"2025-12-30"
+"2025-12-30T13:24:54.456"
+d1 = "5/24/2004"
+d2 = "2025-12-30"
+# d1 < d2
+date1 = datetime.datetime.strptime(d1, "%m/%d/%Y")
+print(d1, date1)
 
-print(now)
-formatted_date = now.strftime("%d/%m/%Y, %A")
-print(type(formatted_date), formatted_date)
+in10days = now + datetime.timedelta(10, hours=37)
+print(in10days)
 
-# dd/mm/yyyy, Tue
+print(in10days.strftime("%d/%m/%Y, %A"))
 
-# 1/2/2024
-# 2024-02-01
+print(in10days.timestamp())
 
-bday = datetime.datetime.strptime(
-    '11-02-2025', '%d-%m-%Y')
-diff = bday - datetime.datetime.now()
-print(diff, type(diff))
-
-print(diff.total_seconds())
-print("hours left:", diff.total_seconds() / 3600)
-
-diff2 = datetime.datetime.now() - \
-        datetime.datetime(1987, 2, 11)
-
-print(diff2)
-
-print(bday < datetime.datetime.now())
-
-print(diff2.days+100)
-
-#01/02/24
-
-ts = 1738669626
+ts = 1747722130
 print(datetime.datetime.fromtimestamp(ts))
+
+#
+# dt = datetime.datetime(1987, 2, 11)
+# print(type(dt))
+# print(days[dt.weekday()])
+#
+# #https://strftime.org/
+# parsed_date = datetime.datetime.strptime(
+#     "2024-02-08, hours: 15:09",
+#     "%Y-%m-%d, hours: %H:%M")
+# print(parsed_date)
+# print(type(parsed_date))
+#
+# print(now)
+# formatted_date = now.strftime("%d/%m/%Y, %A")
+# print(type(formatted_date), formatted_date)
+#
+# # dd/mm/yyyy, Tue
+#
+# # 1/2/2024
+# # 2024-02-01
+#
+# bday = datetime.datetime.strptime(
+#     '11-02-2025', '%d-%m-%Y')
+# diff = bday - datetime.datetime.now()
+# print(diff, type(diff))
+#
+# print(diff.total_seconds())
+# print("hours left:", diff.total_seconds() / 3600)
+#
+# diff2 = datetime.datetime.now() - \
+#         datetime.datetime(1987, 2, 11)
+#
+# print(diff2)
+#
+# print(bday < datetime.datetime.now())
+#
+# print(diff2.days+100)
+#
+# #01/02/24
+#
+# ts = 1738669626
+# print(datetime.datetime.fromtimestamp(ts))
+#
---
+++ meeting3/pack_demo.py
@@ -0,0 +1,15 @@
+# from meeting3.solutions import count_occurences
+# from meeting3.solutions.count_occurences import count_occurrences, result
+# import utils
+
+# from utils import DAYS_IN_YEAR
+# print(type(count_occurences))
+#
+# print(count_occurences.count_occurrences(["w", "e"]))
+
+# print(count_occurrences(["w", "e"]))
+# print(result)
+
+import math
+
+
